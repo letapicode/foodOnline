b0ee6d245fe616231b3b089b53b19c11957e9e98
Ram Adhikari <koncusnai@gmail.com>
2023-01-17 18:51:36 -0500

    cart functionalities

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,7 @@ from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 from django.core.exceptions import PermissionDenied
+from django.template.defaultfilters import slugify
 
 # Create your views here.
 
@@ -96,6 +97,8 @@ def registerVendor(request):
             user.save()
             vendor = v_form.save(commit=False)
             vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -19,11 +19,18 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static 
 
+from marketplace import views as MarketplaceViews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     #For 20 in accounts/urls.py
     path('', include('accounts.urls')),
+
+    path('marketplace/', include('marketplace.urls')),
+
+    # CART
+    path('cart/', MarketplaceViews.cart, name='cart'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
