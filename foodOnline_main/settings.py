352b48650419868c4a92526ce8995056b3416496
Ram Adhikari <koncusnai@gmail.com>
2023-01-23 00:44:06 -0500

    deploy to linode server

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['173.255.225.123','127.0.0.1', 'tokne.online', 'www.tokne.online']
 
 
 # Application definition
