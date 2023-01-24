7388ac2a7c7b79e69831f464d55f9b081a9591f5
Ram Adhikari <koncusnai@gmail.com>
2023-01-23 21:33:53 -0500

    Commented out razorpay code because the api client and key expired. Razorpay is only for India

--- orders/views.py
+++ orders/views.py
@@ -10,13 +10,15 @@ import simplejson as json
 from .utils import generate_order_number, order_total_by_vendor
 from accounts.utils import send_notification
 from django.contrib.auth.decorators import login_required
-import razorpay
-from foodOnline_main.settings import RZP_KEY_ID, RZP_KEY_SECRET
+
+# Razorpay setup
+# import razorpay
+# from foodOnline_main.settings import RZP_KEY_ID, RZP_KEY_SECRET
 from django.contrib.sites.shortcuts import get_current_site
 
 
-
-client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
+# Razorpay setup
+# client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
 
 
 
@@ -90,7 +92,9 @@ def place_order(request):
             order.vendors.add(*vendors_ids)
             order.save()
 
+            # Razorpay setup
             # RazorPay Payment
+            """ 
             DATA = {
                 "amount": float(order.total) * 100,
                 "currency": "INR",
@@ -102,13 +106,15 @@ def place_order(request):
             }
             rzp_order = client.order.create(data=DATA)
             rzp_order_id = rzp_order['id']
+            """
 
             context = {
                 'order': order,
                 'cart_items': cart_items,
-                'rzp_order_id': rzp_order_id,
-                'RZP_KEY_ID': RZP_KEY_ID,
-                'rzp_amount': float(order.total) * 100,
+                # Razorpay setup
+                # 'rzp_order_id': rzp_order_id,
+                # 'RZP_KEY_ID': RZP_KEY_ID,
+                # 'rzp_amount': float(order.total) * 100,
             }
             return render(request, 'orders/place_order.html', context)
 
